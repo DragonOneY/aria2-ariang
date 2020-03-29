e78e692c710ed94c3c7976a91f9074a3a53a312b
DragonOneY <player1311@gmail.com>
2020-03-30 02:39:44 +0800

    update：     conf-temp/delete.sh update：     conf-temp/on-complete.sh

--- conf-temp/on-complete.sh
+++ conf-temp/on-complete.sh
@@ -1,31 +1,9 @@
 #!/bin/sh
-#echo [$(date)] "Download success."
-#echo "Group Id: $1"
-#echo "File Num: $2"
-#echo "File Path: $3"
+echo [$(date)] "Download success."
+echo "Group Id: $1"
+echo "File Num: $2"
+echo "File Path: $3"
 
-#!/bin/bash
 
-downloadpath='aria2/downloads'
 
-filepath=$3
-rdp=${filepath#${downloadpath}/}
-path=${downloadpath}/${rdp%%/*}
-dirpath=${filepath%/*}
 
-if [ $2 -eq 0 ]
-    then
-        exit 0
-elif [ "$path" = "$filepath" ] && [ $2 -eq 1 ]
-    then
-        [ -e "$filepath".aria2 ] && rm -vf "$filepath".aria2
-        exit 0
-elif [ "$path" != "$filepath" ] && [ $2 -gt 1 ]
-    then
-        [ -e "$dirpath".aria2 ] && rm -vf "$dirpath".aria2
-        exit 0
-elif [ "$path" != "$filepath" ] && [ $2 -eq 1 ]
-    then
-        [ -e "$filepath".aria2 ] && rm -vf "$filepath".aria2
-        exit 0
-fi
